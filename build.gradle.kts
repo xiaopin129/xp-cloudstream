bcdd5da15d72ddeb3a02766cd1905da03b45c30f
xiaopin129 <148709744+xiaopin129@users.noreply.github.com>
2026-09-22 16:44:26 +0700

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
+        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
+
+task<Delete>("clean") {
+    delete(rootProject.buildDir)
+}

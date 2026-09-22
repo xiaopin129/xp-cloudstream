143f442d565c7c3c5ff69e5966e4005b1397f386
xiaopin129 <148709744+xiaopin129@users.noreply.github.com>
2026-09-22 17:52:36 +0700

    Update Plugin.kt

--- NguonCProvider/src/main/kotlin/com/nguonc/Plugin.kt
+++ NguonCProvider/src/main/kotlin/com/nguonc/Plugin.kt
@@ -5,7 +5,7 @@ import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
 @CloudstreamPlugin
-class NguonCPlugin: Plugin() {
+class NguonCPlugin : Plugin() {
     override fun load(context: Context) {
         registerMainAPI(NguonCProvider())
     }

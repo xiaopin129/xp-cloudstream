794a58099867fa46b89ebf5f81740fc24cb1f49c
xiaopin129 <148709744+xiaopin129@users.noreply.github.com>
2026-09-22 18:07:59 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,11 @@ buildscript {
     }
 }
 
+plugins {
+    id("com.android.library") version "8.2.2" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
+}
+
 allprojects {
     repositories {
         google()

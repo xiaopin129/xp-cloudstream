35e8878eb73eb3ddd011aea28cb919c2c4cea223
xiaopin129 <148709744+xiaopin129@users.noreply.github.com>
2026-09-22 18:10:11 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,3 @@
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://jitpack.io")
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.1.1")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.20")
-        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
-    }
-}
-
 plugins {
     id("com.android.library") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "2.0.0" apply false
@@ -22,8 +9,4 @@ allprojects {
         mavenCentral()
         maven("https://jitpack.io")
     }
-}
-
-task<Delete>("clean") {
-    delete(rootProject.buildDir)
 }

ee97c77031e6d66fbf63a312ebf75a29cfb1850a
xiaopin129 <148709744+xiaopin129@users.noreply.github.com>
2026-09-22 16:45:51 +0700

    Create build.gradle.kts

---
+++ NguonCProvider/build.gradle.kts
@@ -0,0 +1,30 @@
+apply(plugin = "com.android.library")
+apply(plugin = "kotlin-android")
+apply(plugin = "com.lagradost.cloudstream3.gradle")
+
+android {
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 21
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+dependencies {
+    val cloudstream by configurations
+    cloudstream("com.lagradost:cloudstream3:pre-release")
+}
+
+task<com.lagradost.cloudstream3.gradle.BuildTask>("make") {
+    authors = listOf("YourName")
+    description = "Nguồn C Provider"
+}
